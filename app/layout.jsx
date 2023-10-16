c05a456b9193d5b8e7b997bbe43b0871e49d57d6
Fardin376 <ifardin72@gmail.com>
2023-10-16 15:55:52 +0600

    fix typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 
 export const metadata = {
-  title: 'PromptoAI',
+  title: 'Blog Prompts',
   description: 'Discover & Share AI Prompts',
 };
 
